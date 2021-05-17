044febbd0a91713c697ea797f3347aa49c0baf12
Ivens Muller <ivensdiegomuller@gmail.com>
2021-05-17 20:50:47 -0300

    Remove untracked files

--- backend/database/config/config.example.js
+++ backend/database/config/config.example.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "database_development",
+    "password": "",
+    "database": "DH",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
